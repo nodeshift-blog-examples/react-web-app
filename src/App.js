0ef98270bec22e6df09599d10434b30a1eeb870a
LOUISE ELLIOTT <louise@uk.ibm.com>
2019-11-26 17:25:24 +0000

    Add Carbon Design packages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import MenuSideNav from './MenuSideNav';
 
 class App extends Component {
   render() {
@@ -20,6 +21,7 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        <MenuSideNav/> 
       </div>
     );
   }
---
+++ src/MenuSideNav.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import { SideNav, SideNavItems, SideNavMenu, SideNavMenuItem } from 'carbon-components-react';
+
+/**
+ * Display SideNav page
+ *
+ * @visibleName SideNav
+ */
+class MenuSideNav extends Component {
+
+    render() {
+        return (
+            <SideNav
+                className="menu-side-nav"
+                isFixedNav
+                expanded={true}
+                isChildOfHeader={false}
+                aria-label="Side navigation">
+                <SideNavItems>
+                    <SideNavMenu title="Community Topics" defaultExpanded={true}>
+                        <SideNavMenuItem href="/devops">
+                            DevOps
+                        </SideNavMenuItem>
+                    </SideNavMenu>
+                    <SideNavMenu title="Service Management" defaultExpanded={true} className="side-nav-indent">
+                        <SideNavMenuItem href="/csmo">
+                            CSMO
+                        </SideNavMenuItem>
+                        <SideNavMenuItem href="/tradit">
+                            Traditional IT
+                        </SideNavMenuItem>
+                    </SideNavMenu>
+                    <SideNavMenu title="IBM Cloud Paks" defaultExpanded={true} className="side-nav-indent">
+                        <SideNavMenuItem href="https://www.ibm.com/cloud/cloud-pak-for-applications">
+                            Applications
+                        </SideNavMenuItem>
+                        <SideNavMenuItem href="/mcm" >
+                            Multicloud Management
+                        </SideNavMenuItem>
+                        <SideNavMenuItem href="/system">
+                            System
+                        </SideNavMenuItem>
+                    </SideNavMenu>
+                    <SideNavMenu title="Other IBM Cloud Paks" defaultExpanded={false}>
+                        <SideNavMenuItem href="https://www.ibm.com/cloud/cloud-pak-for-automation">
+                            Automation
+                        </SideNavMenuItem>
+                        <SideNavMenuItem href="https://www.ibm.com/cloud/cloud-pak-for-integration">
+                            Integration
+                        </SideNavMenuItem>
+                        <SideNavMenuItem href="https://www.ibm.com/products/cloud-pak-for-data">
+                            Data
+                        </SideNavMenuItem>
+                        <SideNavMenuItem href="https://www.ibm.com/products/cloud-pak-for-security">
+                            Security
+                        </SideNavMenuItem>
+                    </SideNavMenu>
+                </SideNavItems>
+            </SideNav>
+
+        );
+    }
+}
+
+
+
+
+export default MenuSideNav;
